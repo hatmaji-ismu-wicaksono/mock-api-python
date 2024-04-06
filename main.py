7af628d064af99104d2b2736369f6dad9bd15688
Hatmaji Ismu Wicaksono <hatmaji.ismu@gmail.com>
2024-04-06 23:56:09 +0700

    edit routing for utils

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sanic.response import json
 app = Sanic(__name__)
 
 
-@app.route(uri="/", methods=["GET","POST","PUT","DELETE","OPTIONS","PATCH","HEAD"])
+@app.route(uri="/api/v1/utils/check-headers", methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD"])
 async def getHandler(request):
     """
     This function reads all headers from the request and returns them as a dictionary.
@@ -13,6 +13,14 @@ async def getHandler(request):
     headers["method"] = request.method
     headers["headers"] = dict(request.headers.items())
     return json(headers)
+
+
+@app.route(uri="/api/v1/utils/ping", methods=["GET"])
+async def pingHandler(request):
+    data = {}
+    data["status"] = 200
+    data["message"] = "Pong!"
+    return json(data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
